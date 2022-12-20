528b37e868f36ef08d2e4aa9c45d4a18c8dc09d1
unknown <somnath.bhunia.00@gmail.com>
2022-12-21 00:22:44 +0530

    Inital Commit

---
+++ public/assets/scripts/custom.js
@@ -0,0 +1,11 @@
+$(function(){
+ // $("#leftmenu").load("left-sidebar.html"); 
+  $('.datatable').dataTable( {
+  "searching": false,
+  "lengthChange": false,
+  "info": false,
+  "ordering": false,
+  "paging": false,
+});
+});
+
